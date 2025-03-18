2427a1fab63505a198e7ea9d7a979419619f7034
Dinithi Piyathilaka <dinipiyathilaka@gmail.com>
2025-03-18 11:14:25 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,32 @@
 import express from "express";
 import { PORT , mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-
-
+import materialRoute from './routes/materialRoute.js'; 
+import cors from 'cors';
 
 const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
 app.get('/',(request, response)=>{
     console.log(request)
     return response.status(234).send("welcome")
 });
 
+app.use('/materials', materialRoute);
 
 mongoose.connect(mongoDBURL)
 .then(() => {
