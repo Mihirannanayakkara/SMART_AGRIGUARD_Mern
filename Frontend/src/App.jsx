561b31bdd0403f3f99121a746d70e0035cbbe7ea
Dinithi Piyathilaka <dinipiyathilaka@gmail.com>
2025-03-20 19:51:00 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomeMaterial from './pages/HomeMaterial';
+import CreateMaterial from './pages/CreateMaterial';
+import ShowMaterial from './pages/ShowMaterial';
+ import EditMaterial from './pages/EditMaterial';
+// import DeleteMaterial from './pages/DeleteMaterial';
+
+
 
 const App = () => {
   return (
-    <div className="bg-red-400 text-white">hello</div>
-  )
-}
+    <Routes>
+      <Route path='/' element={<HomeMaterial />} />
+      <Route path='/materials/create' element={<CreateMaterial />} />
+      <Route path='/materials/details/:id' element={<ShowMaterial />} />
+     <Route path='/materials/edit/:id' element={<EditMaterial />} /> 
+      {/* <Route path='/materials/delete/:id' element={<DeleteMaterial />} /> */}
+ 
+        
+    </Routes>
+  );
+};
 
-export default App
+export default App;
